cb0f48e9d0608c7b974b4268ea850fe274ea96a4
smoolinq <riot05550@gmail.com>
2026-02-21 15:47:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 export default {
   async fetch(request) {
+
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        status: 204,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type"
+        }
+      });
+    }
+
+    const corsHeaders = {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    };
+
     if (request.method === "POST") {
       const data = await request.json();
       return new Response(JSON.stringify({ received: data }), {
-        headers: { "Content-Type": "application/json" }
+        headers: corsHeaders
       });
     }
 
     if (request.method === "GET") {
       return new Response(JSON.stringify({ status: "ok" }), {
-        headers: { "Content-Type": "application/json" }
+        headers: corsHeaders
       });
     }
 
-    return new Response("Method Not Allowed", { status: 405 });
+    return new Response("Method Not Allowed", { status: 405, headers: corsHeaders });
   }
 };
